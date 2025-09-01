9270e40c840f4b0d6e4b12e6a86694e510525b1a
savebytes <ankit.singh4x@gmail.com>
2025-09-01 15:30:29 +0530

    sds

--- app/src/main/java/com/example/memoryexe/AdapterSearchItem.kt
+++ app/src/main/java/com/example/memoryexe/AdapterSearchItem.kt
@@ -1,6 +1,6 @@
 package com.example.memoryexe
 
-import androidx.recyclerview.widget.RecyclerView
-
-class AdapterSearchItem: RecyclerView {
-}
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class AdapterSearchItem: RecyclerView {
+//}
